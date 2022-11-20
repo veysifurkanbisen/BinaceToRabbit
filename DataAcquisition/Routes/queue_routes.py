952fa3ef4acd9c4e3641ae70defa22751a487dd3
Veysi Furkan Bisen <veysifurkan.bisen@kocdigital.com>
2022-11-20 04:15:39 +0300

    Completed All

--- DataAcquisition/Routes/queue_routes.py
+++ DataAcquisition/Routes/queue_routes.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter, Body, Request, Response, HTTPException, status
 from fastapi.encoders import jsonable_encoder
 from typing import List
-import json
 
 from Models.QueueModel import Queue
 from Services.queue_service import UserQueue
@@ -37,46 +36,3 @@ def stop_queue(request: Request, queue: Queue = Body(...)):
         "Data": queue_json
     }
     return response
-# @router.get("/", response_description="List all ", response_model=List[Book])
-# def list_books(request: Request):
-#     books = list(request.app.database["books"].find(limit=100))
-#     return books
-
-
-# @router.get("/{id}", response_description="Get a single book by id", response_model=Book)
-# def find_book(id: str, request: Request):
-#     if (book := request.app.database["books"].find_one({"_id": id})) is not None:
-#         return book
-
-#     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Book with ID {id} not found")
-
-
-# @router.put("/{id}", response_description="Update a book", response_model=Book)
-# def update_book(id: str, request: Request, book: BookUpdate = Body(...)):
-#     book = {k: v for k, v in book.dict().items() if v is not None}
-
-#     if len(book) >= 1:
-#         update_result = request.app.database["books"].update_one(
-#             {"_id": id}, {"$set": book}
-#         )
-
-#         if update_result.modified_count == 0:
-#             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Book with ID {id} not found")
-
-#     if (
-#         existing_book := request.app.database["books"].find_one({"_id": id})
-#     ) is not None:
-#         return existing_book
-
-#     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Book with ID {id} not found")
-
-
-# @router.delete("/{id}", response_description="Delete a book")
-# def delete_book(id: str, request: Request, response: Response):
-#     delete_result = request.app.database["books"].delete_one({"_id": id})
-
-#     if delete_result.deleted_count == 1:
-#         response.status_code = status.HTTP_204_NO_CONTENT
-#         return response
-
-#     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Book with ID {id} not found")
